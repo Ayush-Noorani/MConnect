aa2397c1f272f043978a6d95e93a2e97d697094a
Ayush <ayushnoorani6@gmail.com>
2024-07-03 23:26:18 +0530

    adjust icon width and height

--- app/components/Details.jsx
+++ app/components/Details.jsx
@@ -1,18 +1,19 @@
 import React from "react";
+import Image from "next/image";
 
 const Details = () => {
   return (
     <div className="flex flex-col gap-3">
       <div className="flex gap-3">
-        <img src="/location.png" className="w-[4%]" />
+        <Image src="/location.png" width="25" height="5" alt="" />
         <span className="text-lg">Goa | India</span>
       </div>
       <div className="flex gap-3">
-        <img src="/mail.png" className="w-[4%]" />
+        <Image src="/mail.png" width="25" height="5" alt="" />
         <span className="text-lg">ayushnoorani6@gmail.com</span>
       </div>
       <div className="flex gap-3">
-        <img src="/telephone.png" className="w-[4%]" />
+        <Image src="/telephone.png" width="25" height="5" alt="" />
         <span className="text-lg">+91 7709786655</span>
       </div>
     </div>
